54d7bcb4a99e7510d724dccea92828c171ed6dad
llatino <tankawee.boonpeng@gmail.com>
2026-01-28 13:48:00 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    Configuration, ApiClient, MessagingApi, 
+    ReplyMessageRequest, TextMessage
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+
+app = Flask(__name__)
+
+# 1. Add your credentials from the LINE Developers Console
+configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    user_text = event.message.text
+    
+    # 2. Add your Laundry Business Logic here
+    reply_text = "I don't understand that yet. Try clicking a menu button!"
+    
+    if user_text == "Check Status":
+        reply_text = "Your laundry is currently: 🧺 Washing"
+    elif user_text == "Check Bill":
+        reply_text = "Your total is: 150 THB"
+
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply_text)]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run(port=5000)
